ec25ef05b8bdc3597a7ad1a44372763f9e39e7b0
vancityjun <vancityjun@github.com>
2019-07-15 21:05:19 -0700

    Updates

---
+++ precache-manifest.8b4b4dcbb459cf4e48803d39d21c5234.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a13e6078edffb7022d8da844fdec98d",
+    "url": "/perry_sherry/index.html"
+  },
+  {
+    "revision": "7d9b61d1608fb6918168",
+    "url": "/perry_sherry/static/css/2.30cf01b0.chunk.css"
+  },
+  {
+    "revision": "0cdf8ed4fea4c6509754",
+    "url": "/perry_sherry/static/css/main.9fce9ab3.chunk.css"
+  },
+  {
+    "revision": "7d9b61d1608fb6918168",
+    "url": "/perry_sherry/static/js/2.bae764e8.chunk.js"
+  },
+  {
+    "revision": "0cdf8ed4fea4c6509754",
+    "url": "/perry_sherry/static/js/main.1df8f05a.chunk.js"
+  },
+  {
+    "revision": "739cb5dd63ab51293209",
+    "url": "/perry_sherry/static/js/runtime~main.01fd98dd.js"
+  },
+  {
+    "revision": "ba9d1cf553588867fb3db11c04a92fb5",
+    "url": "/perry_sherry/static/media/_DSC0434-Edit.ba9d1cf5.jpg"
+  },
+  {
+    "revision": "7b2c51c1670feb24661e1d503d0802b3",
+    "url": "/perry_sherry/static/media/_DSC0448.7b2c51c1.jpg"
+  },
+  {
+    "revision": "69c4ac743bb2e35b888df2fa98184782",
+    "url": "/perry_sherry/static/media/_DSC0567-Edit.69c4ac74.jpg"
+  },
+  {
+    "revision": "4f9b7a07053f3e8850431896c00ae35c",
+    "url": "/perry_sherry/static/media/_DSC0668.4f9b7a07.jpg"
+  },
+  {
+    "revision": "e8b2b1a44a4c61be54c894604913c79b",
+    "url": "/perry_sherry/static/media/_DSC0745-Edit.e8b2b1a4.jpg"
+  },
+  {
+    "revision": "e1634331d43da155ce49b0e27878df19",
+    "url": "/perry_sherry/static/media/_DSC0848-Edit.e1634331.jpg"
+  },
+  {
+    "revision": "e437f11940236ca51800f1d611e3dcf6",
+    "url": "/perry_sherry/static/media/_DSC0916-Edit-6.e437f119.jpg"
+  },
+  {
+    "revision": "179422a37596dacab8e40188f969e940",
+    "url": "/perry_sherry/static/media/_DSC0974-Edit.179422a3.jpg"
+  },
+  {
+    "revision": "b1e834cfc1d19f1858b0c68496e17130",
+    "url": "/perry_sherry/static/media/_DSC1019.b1e834cf.jpg"
+  },
+  {
+    "revision": "2b13823d1a1bcd8491350c60126e306b",
+    "url": "/perry_sherry/static/media/_DSC2389.2b13823d.jpg"
+  },
+  {
+    "revision": "da298be15e1a6318cffb5b69903c94f2",
+    "url": "/perry_sherry/static/media/_DSC2568-Edit.da298be1.jpg"
+  },
+  {
+    "revision": "1ece1453477203bc216eb9f623f9692f",
+    "url": "/perry_sherry/static/media/_DSC2680.1ece1453.jpg"
+  },
+  {
+    "revision": "6d81bf49fcbee789f2cc27186058cc0f",
+    "url": "/perry_sherry/static/media/_DSC2709-Edit.6d81bf49.jpg"
+  },
+  {
+    "revision": "43844a6bfcb617058ae347965badd3a9",
+    "url": "/perry_sherry/static/media/_DSC2788-Edit.43844a6b.jpg"
+  },
+  {
+    "revision": "08a37fa41b244f5f362137ff6dd6c92d",
+    "url": "/perry_sherry/static/media/_DSC3155-Edit.08a37fa4.jpg"
+  },
+  {
+    "revision": "43844a6bfcb617058ae347965badd3a9",
+    "url": "/perry_sherry/static/media/main.43844a6b.jpg"
+  },
+  {
+    "revision": "6968bb3c4acf44c52bf25f545e2d97f2",
+    "url": "/perry_sherry/static/media/rsvp-01.6968bb3c.png"
+  }
+]);
