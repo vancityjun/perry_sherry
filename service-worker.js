e503ea44db33355dbc7fcf966503fcafedc68161
vancityjun <vancityjun@github.com>
2019-07-24 23:28:39 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1d3cf2c717e4e4781ade2a5e17786638.js"
+  "/precache-manifest.d0bfa8d4b1fbc10e4e0ea1e540d46499.js"
 );
 
 self.addEventListener('message', (event) => {
