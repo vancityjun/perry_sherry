0800eafa7cd413daee47e0cfea4850e6c8ef446b
vancityjun <vancityjun@github.com>
2019-07-27 01:53:21 -0700

    dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,44 +2,51 @@ import React, {Component} from 'react';
 import Header from '../components/Header';
 import firebase from 'firebase';
 import {postActions} from '../actions/postActions';
+import db from "../firebase/config.js"
+import moment from 'moment'
 
-class Admin extends Component{
-		constructor(props){
-		super(props);
-		this.state = {
-			mode:'dashboard',
+class Dashboard extends Component{
+  constructor(props) {
+    super(props);
+    this.ref = firebase.firestore().collection('rsvps');
+    this.state = {
       rsvps: []
-		}
-
-	}
-  componentWillMount(){
-
-    firebase.database().ref().child('rsvps').on('value', snap => { //child_added
-        // console.log(snap.toJSON());
-        console.log(snap.val());
-        const rsvps = () => (
-  <ul>
-    {snap.val().map(guest => (
-      <li>
-        <div>{guest}</div>
-        <div>{guest.firstname}</div>
-        <div>{guest.lastname}</div>
-      </li>
-    ))}
-  </ul>
-);
-    });
+    };
+    this.unsubscribe = null;
   }
 
+    onCollectionUpdate = (querySnapshot) => {
+    const rsvps = [];
+    querySnapshot.forEach((doc) => {
+      const { firstname, lastname, regdate } = doc.data();
+      rsvps.push({
+        key: doc.id,
+        doc, // DocumentSnapshot
+        firstname,
+        lastname,
+        regdate,
+      });
+    });
+    this.setState({
+      rsvps
+   });
+  }
+  componentDidMount() {
+    this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate);
+  }
   render(){
-
-    return(
-     <section className={this.state.mode + ' content'}>
-      <span>{this.rsvps}</span>
-      </section>
-      );
-
+    return (
+              <tbody>
+                {this.state.rsvps.map(guest =>
+                  <tr>
+                    <td>{guest.firstname}</td>
+                    <td>{guest.lastname}</td>
+                    <td>{moment(guest.regdate.toDate()).calendar()}</td>
+                  </tr>
+                )}
+              </tbody>
+    );
   }
 }
 
-export default Admin;
+export default Dashboard;
