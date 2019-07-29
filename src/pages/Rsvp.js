234fdfae897d1c68169c3d70642c8ce57c780cd5
vancityjun <vancityjun@github.com>
2019-07-29 11:53:28 -0700

    rsvp

--- src/pages/Rsvp.js
+++ src/pages/Rsvp.js
@@ -6,6 +6,7 @@ import { render } from "react-dom";
 import db from "../firebase/config.js"
 import ReactDom from 'react-dom';
 import Popup from 'react-popup';
+import Select from 'react-select';
 
 
 class Rsvp extends Component{
@@ -16,6 +17,8 @@ class Rsvp extends Component{
 			phone:'',
 			email:'',
 			regdate:'',
+			agree:false,
+			extras:0,
 			mode:'rsvp',
 			rsvps: []
 		};
@@ -30,7 +33,13 @@ class Rsvp extends Component{
   	}
 
 	onChange(e){
-		this.setState({[e.target.name]: e.target.value});
+		const target = e.target;
+    	const value = target.type === 'checkbox' ? target.checked : target.type === 'number' ? Number(target.value) : target.value;  
+
+    	const name = target.name;
+		this.setState({
+      		[name]: value
+    	});
 	}
 	onSubmit(e){
 		e.preventDefault();
@@ -38,7 +47,9 @@ class Rsvp extends Component{
 	        fullname: this.state.fullname,
 	        phone: this.state.phone,
 	        email: this.state.email,
+	        agree: this.state.agree,
 	        regdate: new Date(),
+	        extras: this.state.extras,
 		})
 		.then(function() {
 		    alert("Document successfully written!");
@@ -49,6 +60,15 @@ class Rsvp extends Component{
 	}
 	submit(){
 	}
+ options = [
+  { label: "0", value: 0 },
+  { label: "1", value: 1 },
+  { label: "2", value: 2 },
+  { label: "3", value: 3 },
+  { label: "4", value: 4 },
+  { label: "5", value: 5 },
+  { label: "6", value: 6 },
+];
 
   render(){
     return(
@@ -75,27 +95,13 @@ class Rsvp extends Component{
 	      	<div className="cf">
 	      		<span className="fl">Do you want to receive our invitation card?</span>
 	      		<span className="fc fr">
-					<input type="checkbox" id="agree" name="agree" />
+					<input type="checkbox" id="agree" name="agree" checked={this.state.agree} onChange={this.onChange} />
 					<label for="agree">agree</label>
 	      		</span>
 	      	</div>
 	      	<div className="cf">
 		      	<span className="fl">How many friends with you?</span>
-			      	<span className="fr">
-				      	<select name="extra" id="extra" className="">
-							<option value="0">0</option>
-							<option value="1">1</option>
-							<option value="2">2</option>
-							<option value="3">3</option>
-							<option value="4">4</option>
-							<option value="5">5</option>
-							<option value="6">6</option>
-							<option value="7">7</option>
-							<option value="8">8</option>
-							<option value="9">9</option>
-							<option value="10">10</option>
-				      	</select>
-			      	</span>
+			      	<input className="fr" type="number" id="extras" name="extras" value={this.state.extras} onChange={this.onChange} min="0" max="10" />
 			      	</div>
 	      	<button type="submit">Submit</button>
 	      </form>
